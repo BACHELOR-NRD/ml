be8141be9aa0ee8ba6e6d2863146eb3d08cf792c
Damian Trafialek <damian.trafialek@gmail.com>
2025-09-26 13:48:35 +0200

    add: setup for yolo and gpu

--- ml_carbucks/__init__.py
+++ ml_carbucks/__init__.py
@@ -4,6 +4,13 @@ from pathlib import Path
 
 RESULTS_DIR = Path(os.path.abspath(__file__)).parent.parent / "results"
 
-DATA_CAR_DD_DIR = Path(os.path.abspath(__file__)).parent.parent / "data" / "car_dd"
+DATA_DIR = Path(os.path.abspath(__file__)).parent.parent / "data"
+DATA_CAR_DD_DIR = DATA_DIR / "car_dd"
 DATA_CAR_DD_YAML = DATA_CAR_DD_DIR / "dataset.yaml"
 DATA_CAR_DD_TEST_IMAGES_DIR = DATA_CAR_DD_DIR / "images" / "test"
+
+YOLO_PRETRAINED_11L = DATA_DIR / "yolo_models" / "yolo11l.pt"
+YOLO_PRETRAINED_11N = DATA_DIR / "yolo_models" / "yolo11n.pt"
+
+
+os.makedirs(RESULTS_DIR, exist_ok=True)
