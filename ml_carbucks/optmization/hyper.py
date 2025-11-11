54029d8bcb3ddabb4e91a9fa8f11b38ca67e899a
Damian Trafialek <damian.trafialek@gmail.com>
2025-11-11 16:08:41 +0100

    add: restructured optimization

--- ml_carbucks/optmization/hyper.py
+++ ml_carbucks/optmization/hyper.py
@@ -1,11 +1,9 @@
-import json
 import datetime as dt
 from pathlib import Path
 from functools import partial
-from typing import Callable, Optional
+from typing import Optional
 import warnings
 
-import optuna
 import pandas as pd
 
 
@@ -16,7 +14,7 @@ from ml_carbucks.adapters.UltralyticsAdapter import (  # noqa: F401
 )
 from ml_carbucks.adapters.FasterRcnnAdapter import FasterRcnnAdapter  # noqa: F401
 from ml_carbucks.adapters.EfficientDetAdapter import EfficientDetAdapter  # noqa: F401
-from ml_carbucks.optmization.EarlyStoppingCallback import create_early_stopping_callback
+from ml_carbucks.optmization.simple_study import execute_simple_study
 from ml_carbucks.optmization.objective import create_objective
 from ml_carbucks.utils.logger import setup_logger
 from ml_carbucks import DATA_DIR, RESULTS_DIR
@@ -27,83 +25,6 @@ warnings.filterwarnings(
     "ignore",
     message="grid_sampler_2d_backward_cuda does not have a deterministic implementation",
 )
-
-
-def execute_simple_study(
-    name: str,
-    results_dir: Path,
-    n_trials: int,
-    create_objective_func: Callable,
-    adapter: BaseDetectionAdapter,
-    patience: int = -1,
-    min_percentage_improvement: float = 0.01,
-    optimization_timeout: Optional[float] = None,
-):
-    result_dir_path = results_dir / "optuna" / f"hyper_{name}"
-    result_dir_path.mkdir(parents=True, exist_ok=True)
-
-    sql_path = (
-        results_dir / "optuna" / f"study_{name}" / f"{adapter.__class__.__name__}.db"
-    )
-    sql_path.parent.mkdir(parents=True, exist_ok=True)
-
-    study = optuna.create_study(
-        direction="maximize",
-        study_name=name,
-        load_if_exists=True,
-        storage=f"sqlite:///{sql_path}",
-    )
-
-    early_callback = None
-    if patience > 0:
-        early_callback = create_early_stopping_callback(
-            name=name,
-            study=study,
-            patience=patience,
-            min_percentage_improvement=min_percentage_improvement,
-        )
-
-    study.optimize(
-        func=create_objective_func(adapter),
-        n_trials=n_trials,
-        callbacks=[early_callback] if early_callback else None,
-        timeout=optimization_timeout,
-        gc_after_trial=True,
-    )
-
-    best_trial = None
-    completed_trials = [
-        trial
-        for trial in study.trials
-        if trial.state == optuna.trial.TrialState.COMPLETE
-    ]
-    if len(completed_trials) == 0:
-        logger.error(f"No completed trials found for model combination {name}")
-
-    is_single_objective = study.directions is None or len(study.directions) == 1
-    if not is_single_objective:
-        # NOTE: ROBUST
-        best_trial = max(study.best_trials, key=lambda t: min(t.values))
-    else:
-        best_trial = study.best_trial
-
-    # NOTE: the best model would have to be retrained so that it could be saved properly
-    # model_path = adapter.save(result_dir_path)
-    hyper_results = {
-        "best_params": best_trial.params,
-        "best_value": best_trial.value if is_single_objective else best_trial.values[0],
-        # "model_path": model_path,
-        "classes": adapter.classes,
-        "best_trial_number": best_trial.number,
-        "study_name": name,
-        "adapter": adapter.__class__.__name__,
-        "n_trials": len(completed_trials),
-    }
-
-    with open(result_dir_path / f"results_{adapter.__class__.__name__}.json", "w") as f:
-        json.dump(hyper_results, f)
-
-    return hyper_results
 
 
 def main(
@@ -120,24 +41,33 @@ def main(
     results = []
     for adapter in adapter_list:
         result = execute_simple_study(
-            name=runtime,
+            hyper_name=runtime,
+            study_name=adapter.__class__.__name__,
             results_dir=results_dir,
             n_trials=n_trials,
             create_objective_func=partial(
                 create_objective,
+                adapter=adapter,
                 train_datasets=train_datasets,
                 val_datasets=val_datasets,
-                results_dir=results_dir / "optuna" / f"hyper_{runtime}" / "checkpoints",
+                results_dir=results_dir / "optuna" / "hyper" / f"checkpoints_{runtime}",
             ),
-            adapter=adapter,
             patience=patience,
             min_percentage_improvement=min_percentage_improvement,
             optimization_timeout=optimization_timeout,
+            metadata={
+                "runtime": runtime,
+                "train_datasets": train_datasets,
+                "val_datasets": val_datasets,
+                "adapter": adapter.__class__.__name__,
+            },
         )
         results.append(result)
 
     df = pd.DataFrame(results)
-    df.to_csv(results_dir / "optuna" / f"aggregated_hyper_{runtime}.csv", index=False)
+    aggregated_results_path = results_dir / "optuna" / f"aggregated_hyper_{runtime}.csv"
+    df.to_csv(aggregated_results_path, index=False)
+    logger.info(f"Aggregated results saved to {aggregated_results_path}")
 
 
 if __name__ == "__main__":
@@ -145,17 +75,9 @@ if __name__ == "__main__":
     runtime = dt.datetime.now().strftime("%Y%m%d_%H%M%S")
     main(
         adapter_list=[
-            RtdetrUltralyticsAdapter(
-                classes=classes,
-                name="rtdetr",
-                project_dir=RESULTS_DIR / f"optuna_ultralytics_{runtime}",
-            ),
+            RtdetrUltralyticsAdapter(classes=classes, training_save=False),
             EfficientDetAdapter(classes=classes),
-            YoloUltralyticsAdapter(
-                classes=classes,
-                name="yolo",
-                project_dir=RESULTS_DIR / f"optuna_ultralytics_{runtime}",
-            ),
+            YoloUltralyticsAdapter(classes=classes, training_save=False),
             FasterRcnnAdapter(classes=classes),
         ],
         runtime=runtime,
---
+++ ml_carbucks/optmization/simple_study.py
@@ -0,0 +1,99 @@
+import json
+from pathlib import Path
+from typing import Callable, Optional
+
+import optuna
+
+from ml_carbucks.optmization.EarlyStoppingCallback import create_early_stopping_callback
+from ml_carbucks.utils.logger import setup_logger
+
+logger = setup_logger(__name__)
+
+
+def execute_simple_study(
+    hyper_name: str,
+    study_name: str,
+    results_dir: Path,
+    n_trials: int,
+    create_objective_func: Callable,
+    patience: int = -1,
+    min_percentage_improvement: float = 0.01,
+    optimization_timeout: Optional[float] = None,
+    metadata: Optional[dict] = None,
+):
+    if metadata is None:
+        metadata = {}
+
+    metadata.update(
+        {
+            "hyper_name": hyper_name,
+            "study_name": study_name,
+            "n_trials": n_trials,
+            "patience": patience,
+            "min_percentage_improvement": min_percentage_improvement,
+            "optimization_timeout": optimization_timeout,
+            "results_dir": str(results_dir),
+        }
+    )
+
+    hyper_dir_path = results_dir / "optuna" / "hyper"
+    hyper_dir_path.mkdir(parents=True, exist_ok=True)
+
+    sql_path = results_dir / "optuna" / "studies" / f"{study_name}.db"
+    sql_path.parent.mkdir(parents=True, exist_ok=True)
+
+    study = optuna.create_study(
+        direction="maximize",
+        study_name=study_name,
+        load_if_exists=True,
+        storage=f"sqlite:///{sql_path}",
+    )
+
+    early_callback = None
+    if patience > 0:
+        early_callback = create_early_stopping_callback(
+            name=study_name,
+            study=study,
+            patience=patience,
+            min_percentage_improvement=min_percentage_improvement,
+        )
+
+    study.optimize(
+        func=create_objective_func(),
+        n_trials=n_trials,
+        callbacks=[early_callback] if early_callback else None,
+        timeout=optimization_timeout,
+        gc_after_trial=True,
+    )
+
+    best_trial = None
+    completed_trials = [
+        trial
+        for trial in study.trials
+        if trial.state == optuna.trial.TrialState.COMPLETE
+    ]
+    if len(completed_trials) == 0:
+        logger.error(
+            f"No completed trials found for model combination {hyper_name}-{study_name}."
+        )
+
+    is_single_objective = study.directions is None or len(study.directions) == 1
+    if not is_single_objective:
+        # NOTE: ROBUST
+        best_trial = max(study.best_trials, key=lambda t: min(t.values))
+    else:
+        best_trial = study.best_trial
+
+    hyper_results = {
+        "best_params": best_trial.params,
+        "best_value": best_trial.value if is_single_objective else best_trial.values[0],
+        "best_trial_number": best_trial.number,
+        "study_name": study_name,
+        "n_trials": len(completed_trials),
+        **metadata,
+    }
+
+    with open(hyper_dir_path / f"results_{hyper_name}_{study_name}.json", "w") as f:
+        json.dump(hyper_results, f)
+
+    return hyper_results
